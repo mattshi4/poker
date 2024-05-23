8c5e245c14c8fb93d90f7f10b79c2898a777d5d9
Matthew Shi <mxueqianshi@gmail.com>
2024-05-23 18:32:43 +1000

    Added an env file to hide important components for future

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 // Third party packages
+require('dotenv').config();
 const express = require('express');
 
 // This will now start up the express app
@@ -6,12 +7,12 @@ const app = express();
 
 // Routes
 app.get('/', (req, res) => {
-   res.json(msg: 'welcome to the app');
+   res.json( {msg: 'welcome to the app siuuu'} );
 })
 
 // listen for requests
-app.listen(4000, () => {
-   console.log('listening on port 4000')
+app.listen(process.env.PORT, () => {
+   console.log(`listening on port ${process.env.PORT}`)
 });
 
 
